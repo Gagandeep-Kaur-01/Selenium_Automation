738b4be593e5b47d3de12a7d0c6e1d23f0788a53
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-08-02 22:53:04 +0530

    starting selenium automation testing

---
+++ src/automation_test/My_First_Prog.java
@@ -0,0 +1,14 @@
+package automation_test;
+
+public class My_First_Prog {
+	
+	public static void main (String[]args)
+	{
+		// which browser to use
+		// open the browser
+		// hit the url of any website
+	}
+	
+}
+
+
